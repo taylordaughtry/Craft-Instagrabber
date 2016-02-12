ab9485db954cb8a7b79ad801fc5015f9173e1bd7
Taylor Daughtry <hello@taylordaughtry.com>
2016-02-12 00:27:47 -0600

    Add account connection functionality

---
+++ InstagrabberPlugin.php
@@ -0,0 +1,76 @@
+<?php
+namespace Craft;
+
+class InstagrabberPlugin extends BasePlugin
+{
+	public function getName()
+	{
+		 return 'Instagrabber';
+	}
+
+	public function getDescription()
+	{
+		return 'Integrate Instagram into your client sites.';
+	}
+
+	public function getVersion()
+	{
+		return '1.0.0';
+	}
+
+	public function getSchemaVersion()
+	{
+		return '1.0.0';
+	}
+
+	public function getDeveloper()
+	{
+		return 'Taylor Daughtry';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'https://github.com/taylordaughtry';
+	}
+
+	public function getDocumentationUrl()
+	{
+		return 'https://github.com/taylordaughtry/craft-instagrabber';
+	}
+
+	public function getReleaseFeedUrl()
+	{
+		return '';
+	}
+
+	public function getIconPath()
+	{
+		return craft()->path->getPluginsPath().'instagrabber/resources/icon.svg';
+	}
+
+	public function getSettingsHtml()
+	{
+		return craft()->templates->render('instagrabber/settings', [
+			'settings' => $this->getSettings()
+		]);
+	}
+
+	protected function defineSettings()
+	{
+		return [
+			'clientId' => array(AttributeType::String, 'default' => ''),
+			'clientSecret' => array(AttributeType::String, 'default' => ''),
+			'isConnected' => array(AttributeType::Number, 'default' => 0),
+			'username' => array(AttributeType::String, 'default' => 0),
+			'profile_picture' => array(AttributeType::String, 'default' => 0),
+			'full_name' => array(AttributeType::String, 'default' => 0),
+		];
+	}
+
+	public function registerSiteRoutes()
+	{
+		return [
+			'instagrabber/connect' => ['action' => 'instagrabber/connect'],
+		];
+	}
+}
---
+++ controllers/InstagrabberController.php
@@ -0,0 +1,82 @@
+<?php
+namespace Craft;
+
+class InstagrabberController extends BaseController
+{
+	protected $settings;
+
+	public function __construct()
+	{
+		$this->settings = craft()->plugins->getPlugin('instagrabber')->getSettings();
+	}
+
+    public function actionConnect()
+    {
+    	$code = craft()->request->getQuery('code');
+
+    	$headers = [
+    		'client_id' => $this->settings->clientId,
+    		'client_secret' => $this->settings->clientSecret,
+    		'grant_type' => 'authorization_code',
+    		'redirect_uri' => craft()->getSiteUrl() . 'instagrabber/connect',
+    		'code' => $code,
+    	];
+
+    	$client = new \Guzzle\Http\Client();
+
+    	$url = 'https://api.instagram.com/oauth/access_token';
+
+    	$postData = [
+    		'client_id' => $this->settings->clientId,
+    		'client_secret' => $this->settings->clientSecret,
+    		'grant_type' => 'authorization_code',
+    		'redirect_uri' => craft()->getSiteUrl() . 'instagrabber/connect',
+    		'code' => $code,
+    	];
+
+    	// The null value is an optional array of headers
+    	$request = $client->post($url, null, $postData);
+
+    	try {
+			$response = $request->send();
+
+			// If we got a successful return, tell Craft to save that setting.
+			if ($response->getStatusCode() === 200) {
+				$data = $response->json()['user'];
+
+				$instagrabber = craft()->plugins->getPlugin('instagrabber');
+
+				$settings = [
+					'username' => $data['username'],
+					'profile_picture' => $data['profile_picture'],
+					'full_name' => $data['full_name'],
+					'isConnected' => 1,
+				];
+
+				craft()->plugins->savePluginSettings($instagrabber, $settings);
+			}
+
+		} catch (\Exception $e) {
+			throw new HttpException(400, 'Instagrabber couldn\'t connect to your account.');
+		}
+
+		craft()->userSession->setNotice('Account connected.');
+
+		$this->redirect(UrlHelper::getCpUrl('settings/plugins/instagrabber'));
+    }
+
+    public function actionDisconnect()
+    {
+    	$instagrabber = craft()->plugins->getPlugin('instagrabber');
+
+    	$params = [
+    		'isConnected' => 0,
+    	];
+
+    	craft()->plugins->savePluginSettings($instagrabber, $params);
+
+    	craft()->userSession->setNotice('Account disconnected.');
+
+		$this->redirect(UrlHelper::getCpUrl('settings/plugins/instagrabber'));
+    }
+}
